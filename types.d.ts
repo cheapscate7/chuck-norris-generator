4599fa6bbdb8347f6ee4a43fb7bb85dd28ef4d2c
Matthew Badcock <matthewlisterb@gmail.com>
2020-08-19 19:40:44 +0200

    added theme module + type definition

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+/**
+ * THEME
+ */
+declare interface Theme {
+    colors: {
+        foreground: string;
+        background: string;
+        highlight_1: string;
+        highlight_2?: string;
+        highlight_1_contrast: string;
+        highlight_2_contrast?: string;
+        coloured_button_foreground: string;
+    };
+    fontWeights: {
+        light: number;
+        normal: number;
+        heavy: number;
+        bold: string | number;
+    };
+    fontSizes: {
+        tiny: string;
+        small: string;
+        medium: string;
+        large: string;
+        huge: string;
+    };
+    shadows?: string[];
+}
