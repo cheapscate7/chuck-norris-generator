2a8a533a3f46c7d1c02c41c063bd2dc3a170af89
Matthew Badcock <matthewlisterb@gmail.com>
2020-08-22 21:44:35 +0200

    added Favourites reducer

---
+++ components/buttons/FavouriteButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const FavouriteButton: React.FC = ({ joke }) => {
+    const handleCLick = () => {};
+    return <button>Favourite</button>;
+};
--- pages/random.tsx
+++ pages/random.tsx
@@ -1,8 +1,6 @@
 import { NextPage } from 'next';
 import Layout from '../components/layout/Layout';
-import MenuList from '../components/lists/menu/MenuList';
 import ApplicationContainer from '../components/containers/ApplicationContainer';
-import JokesList from '../components/lists/jokes/JokesList';
 import MemeContainer from '../components/containers/MemeContainer';
 import { SelectedJokeProvider } from '../lib/withJokeSelect';
 import JokesListContainer from '../components/containers/JokesListContainer';
@@ -14,6 +12,7 @@ const pageDescription = 'Find Random Chuck Norris Memes';
 interface IRandomPageProps {
     jokes: IJoke[];
     success: boolean;
+    favourites: IJoke[];
 }
 
 const RandomPage: NextPage<IRandomPageProps> = ({ jokes, success }) => (
@@ -28,15 +27,18 @@ const RandomPage: NextPage<IRandomPageProps> = ({ jokes, success }) => (
 );
 
 RandomPage.getInitialProps = async () => {
-    return await fetch('http://api.icndb.com/jokes/random/10')
-        .then((resp) => resp.json())
-        .then((result) => {
-            return {
-                jokes: result['value'],
-                // success: result['type'] === 'success' && result['value'].length > 0
-                success: true,
-            };
-        });
+    const random_jokes = await fetch(
+        'http://api.icndb.com/jokes/random/10'
+    ).then((resp) => resp.json());
+    const favourites = await fetch(
+        'http://localhost:7777/favourites'
+    ).then((resp) => resp.json());
+    return {
+        jokes: random_jokes['value'],
+        // success: result['type'] === 'success' && result['value'].length > 0
+        success: true,
+        favourites,
+    };
 };
 
 export default RandomPage;
