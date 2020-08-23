270fd67b64480ead0e253868eab0816c0e6e1b52
Matthew Badcock <matthewlisterb@gmail.com>
2020-08-23 15:56:03 +0200

    Added an "Add to favourites" button that changes to "delete from favourites" if the joke is already in favourites

---
+++ components/buttons/AddToFavouritesButton.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import FloatingButton from './FloatingButton';
+import {
+    favouritesActions,
+    useFavouritesDispatch,
+    useFavouritesState,
+} from '../../lib/withFavourites';
+import { useSelectedJokeState } from '../../lib/withJokeSelect';
+import { addJokeToFavourites } from '../../lib/jokes';
+import DeleteFavouriteButton from './DeleteFavouriteButton';
+
+const AddToFavouritesButton: React.FC = () => {
+    const favouritesState = useFavouritesState();
+    const selectedJokeState = useSelectedJokeState();
+
+    const selectedJoke = selectedJokeState.selectedJoke;
+    const favourites = favouritesState.favourites;
+
+    const isInFavourites = favourites.some(
+        (favourite) => favourite.id === selectedJoke.id
+    );
+
+    if (isInFavourites) {
+        return (
+            <DeleteFavouriteButton>
+                Remove From Favourites
+            </DeleteFavouriteButton>
+        );
+    } else {
+        const favouritesDispatch = useFavouritesDispatch();
+        const hidden = !selectedJoke.id;
+        const handleAddToFavourites = () => {
+            addJokeToFavourites(selectedJoke).then((result) => {
+                if (result.success) {
+                    favouritesDispatch(
+                        favouritesActions.addFavourite(selectedJoke)
+                    );
+                }
+            });
+        };
+
+        return (
+            <FloatingButton onClick={handleAddToFavourites} hidden={hidden}>
+                Add to Favourites
+            </FloatingButton>
+        );
+    }
+};
+
+export default AddToFavouritesButton;
